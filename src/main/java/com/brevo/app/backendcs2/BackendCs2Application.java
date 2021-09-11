3bec99013cab4263fed5dcaf18f9710301f3e7b9
Stiven106 <brayan106-@hotmail.com6>
2021-09-11 12:05:25 -0500

    spring init

---
+++ src/main/java/com/brevo/app/backendcs2/BackendCs2Application.java
@@ -0,0 +1,13 @@
+package com.brevo.app.backendcs2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCs2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCs2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/brevo/app/backendcs2/BackendCs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brevo.app.backendcs2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCs2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
